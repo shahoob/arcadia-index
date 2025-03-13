d23421b0f23d70fa508df4e4795f00edbf4c627a
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-03-13 15:17:03 +0100

    registering works

---
+++ src/handlers/auth_handler.rs
@@ -0,0 +1,43 @@
+use std::net::IpAddr;
+
+use actix_web::{HttpRequest, HttpResponse, web};
+use sqlx::PgPool;
+
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHasher, SaltString, rand_core::OsRng},
+};
+
+use crate::{models::user::Register, repositories::auth_repository::create_user};
+
+pub async fn register(
+    user_register: web::Json<Register>,
+    pool: web::Data<PgPool>,
+    req: HttpRequest,
+) -> HttpResponse {
+    let client_ip: IpAddr = req
+        .connection_info()
+        .realip_remote_addr()
+        .and_then(|ip| ip.parse().ok())
+        .unwrap();
+
+    let new_user = Register {
+        ..user_register.into_inner()
+    };
+
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Argon2 with default params (Argon2id v19)
+    let argon2 = Argon2::default();
+
+    // Hash password to PHC string ($argon2id$v=19$...)
+    let password_hash = argon2
+        .hash_password(new_user.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
+
+    match create_user(&pool, &new_user, &client_ip, &password_hash).await {
+        Ok(user) => HttpResponse::Created().json(user),
+        Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
+    }
+}
--- src/handlers/user_handler.rs
+++ src/handlers/user_handler.rs
@@ -1,31 +1,21 @@
-use std::{net::IpAddr, str::FromStr};
+use std::{net::IpAddr, str::FromStr, time::Duration};
 
-use actix_web::{HttpResponse, web};
+use actix_web::{HttpRequest, HttpResponse, web};
+use chrono::Utc;
 
-use crate::models::user::{CreateUser, User};
+use crate::models::user::{Register, User};
 
-pub async fn create_user(user: web::Json<CreateUser>) -> HttpResponse {
-    let new_user = User {
-        id: 0, //TODO
-        username: user.username.clone(),
-        email: user.email.clone(),
-        created_from_ip: IpAddr::from_str("127.0.0.1").unwrap(),
-    };
+// pub async fn get_user(user_id: web::Data<u32>) -> HttpResponse {
+//     // For now, we'll just mock a user response.
+//     let mock_user = User {
+//         id: **user_id,
+//         username: String::from("mock_user"),
+//         email: String::from("mock_user@example.com"),
+//         created_from_ip: IpAddr::from_str("127.0.0.1").unwrap(),
+//     };
 
-    HttpResponse::Created().json(new_user)
-}
-
-pub async fn get_user(user_id: web::Data<u32>) -> HttpResponse {
-    // For now, we'll just mock a user response.
-    let mock_user = User {
-        id: **user_id,
-        username: String::from("mock_user"),
-        email: String::from("mock_user@example.com"),
-        created_from_ip: IpAddr::from_str("127.0.0.1").unwrap(),
-    };
-
-    HttpResponse::Ok().json(mock_user)
-}
+//     HttpResponse::Ok().json(mock_user)
+// }
 
 pub async fn health_check() -> HttpResponse {
     HttpResponse::Ok().json("API is up and running!")
