c3a9ed99af2de4548860ebfaf47828f152979e0a
FrenchGithubUser <thomas_traineau@tutanota.com>
2025-03-20 20:11:54 +0100

    started working on title group view

---
+++ src/components/ContentContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ContentContainer',
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+  border-radius: 10px;
+  background-color: rgb(29, 29, 29);
+}
+</style>
