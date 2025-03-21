efc68b74e971a717c805666304ab8968fde4b3b5
FrenchGithubUser <thomas_traineau@tutanota.com>
2025-03-21 13:16:02 +0100

    title group

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -8,4 +8,14 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token && !config.url?.includes('/login') && !config.url?.includes('/register')) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default api;
