530fc5f15e07e1516d3ac6747ba5eb32ef8c5a06
FrenchGithubUser <thomas_traineau@tutanota.com>
2025-03-27 12:53:52 +0100

    get artist publications

--- src/handlers/artist_handler.rs
+++ src/handlers/artist_handler.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
 use crate::{
     models::{artist::UserCreatedArtist, title_group::UserCreatedAffiliatedArtist, user::User},
-    repositories::artist_repository::{create_artist, create_artists_affiliation},
+    repositories::artist_repository::{
+        create_artist, create_artists_affiliation, find_artist_publications,
+    },
 };
 use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
@@ -26,6 +30,21 @@ pub async fn add_affiliated_artists(
     match create_artists_affiliation(&pool, &artists, &current_user).await {
         Ok(created_affiliations) => {
             HttpResponse::Created().json(serde_json::json!(created_affiliations))
+        }
+        Err(err) => HttpResponse::InternalServerError().json(serde_json::json!({
+            "error": err.to_string()
+        })),
+    }
+}
+
+pub async fn get_artist_publications(
+    query: web::Query<HashMap<String, String>>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let artist_id = query.get("id").expect("id not found in query");
+    match find_artist_publications(&pool, &artist_id.parse::<i32>().unwrap()).await {
+        Ok(artist_publications) => {
+            HttpResponse::Created().json(serde_json::json!(artist_publications))
         }
         Err(err) => HttpResponse::InternalServerError().json(serde_json::json!({
             "error": err.to_string()
