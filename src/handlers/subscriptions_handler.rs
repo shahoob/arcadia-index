9ceb935591c42f26afc017ad7639b304232ca386
FrenchGithubUser <thomas_traineau@tutanota.com>
2025-03-27 21:29:38 +0100

    subscribe/unsubscribe to title_group

--- src/handlers/subscriptions_handler.rs
+++ src/handlers/subscriptions_handler.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
-use crate::{models::user::User, repositories::subscriptions_repository::create_subscription};
+use crate::{
+    models::user::User,
+    repositories::subscriptions_repository::{create_subscription, delete_subscription},
+};
 use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 
@@ -12,6 +15,28 @@ pub async fn add_subscription(
     let item_id = query.get("item_id").expect("id not found in query");
     let item = query.get("item").expect("item not found in query");
     match create_subscription(
+        &pool,
+        &item_id.parse::<i64>().unwrap(),
+        &item,
+        &current_user,
+    )
+    .await
+    {
+        Ok(_) => HttpResponse::Created().json(serde_json::json!({"result": "success"})),
+        Err(err) => HttpResponse::InternalServerError().json(serde_json::json!({
+            "error": err.to_string()
+        })),
+    }
+}
+
+pub async fn remove_subscription(
+    query: web::Query<HashMap<String, String>>,
+    pool: web::Data<PgPool>,
+    current_user: User,
+) -> HttpResponse {
+    let item_id = query.get("item_id").expect("id not found in query");
+    let item = query.get("item").expect("item not found in query");
+    match delete_subscription(
         &pool,
         &item_id.parse::<i64>().unwrap(),
         &item,
