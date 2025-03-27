6d9151614e5f4f6c8c14ca24ad0af94b3c8db666
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-03-27 14:06:52 +0100

    better series list presentation

--- src/components/torrents/TitleGroupPreviewTable.vue
+++ src/components/torrents/TitleGroupPreviewTable.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="title-group-preview-table">
-    <TitleGroupTable :title_group="title_group" :edition_groups="title_group.edition_groups" />
-  </div>
+  <ContentContainer class="title-group-preview-table">
+    <!-- TODO : add tags and other potentially useful information -->
+    <!-- TODO : clicking on a torrent should redirect to the title_group page
+      edit the titlegrouptable component to have a prop that allows this -->
+    <Image :src="title_group.covers[0]" preview class="cover">
+      <template #previewicon>
+        <i class="pi pi-search"></i>
+      </template>
+    </Image>
+    <div class="right">
+      <div class="title">
+        {{ title_group.name }}
+        <span class="year">({{ title_group.original_release_date.substring(0, 4) }})</span>
+      </div>
+      <TitleGroupTable
+        class="table"
+        :title_group="title_group"
+        :edition_groups="title_group.edition_groups"
+      />
+    </div>
+  </ContentContainer>
 </template>
 <script lang="ts">
 import { Image } from 'primevue'
 import TitleGroupTable from './TitleGroupTable.vue'
+import ContentContainer from '../ContentContainer.vue'
 export default {
-  components: { Image, TitleGroupTable },
+  components: { Image, TitleGroupTable, ContentContainer },
   props: {
     title_group: {},
   },
@@ -16,13 +35,32 @@ export default {
   },
 }
 </script>
-
-<style>
-.title-group-preview-cover-only .title-group-cover {
-  height: 20em;
-  border-radius: 7px;
+<style scoped>
+.title-group-preview-table {
+  display: flex;
+  justify-content: center;
 }
-.title-group-preview-cover-only a:hover {
-  background-color: transparent;
+.right {
+  width: 100%;
+}
+.title {
+  font-weight: bold;
+  font-size: 1.5em;
+  margin-bottom: 5px;
+}
+.year {
+  font-size: 0.8em;
+}
+.table {
+  width: 100%;
+}
+</style>
+<style>
+.title-group-preview-table .cover {
+  height: 20em;
+  margin-right: 20px;
+}
+.title-group-preview-table .cover img {
+  border-radius: 7px;
 }
 </style>
