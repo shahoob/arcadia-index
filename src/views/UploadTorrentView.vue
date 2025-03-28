1d9256945777fe94ad1a86d10b27e2dd7c9f9be0
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-03-28 12:19:30 +0100

    started torrent upload form

---
+++ src/views/UploadTorrentView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><CreateOrSelectTitleGroup /></div>
+</template>
+<script lang="ts">
+import CreateOrSelectTitleGroup from '@/components/torrents/CreateOrSelectTitleGroup.vue'
+export default {
+  components: { CreateOrSelectTitleGroup },
+}
+</script>
+
+<style></style>
