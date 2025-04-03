74cb8b416665db2a0b1e1f1bb65ab0990c60a2d3
diceypotato <nice.mouse1085@fastmail.com>
2025-04-03 13:43:59 -0500

    invitations: disallow invite registration post-expiry

---
+++ tests/fixtures/with_expired_test_user_invite.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    invitations (expires_at, invitation_key, message, sender_id, receiver_email)
+VALUES
+    -- invite expired 7 days ago. too late!
+    (CURRENT_TIMESTAMP - INTERVAL '7 days', 'valid_key', 'invitation message', 1, 'newuser@testdomain.com')
