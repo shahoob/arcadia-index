378bf7571a1911d1dbe31eb351c5a6b5dd5a1092
diceypotato <nice.mouse1085@fastmail.com>
2025-04-05 15:59:57 -0500

    torrent: wire up basic TMDB support for movies and TV

--- src/services/api/externalDatabasesService.ts
+++ src/services/api/externalDatabasesService.ts
@@ -6,6 +6,12 @@ export const getExternalDatabaseData = async (item_id: string | Number, database
       case 'openlibrary': {
         return (await api.get('external_db/open_library?id=' + item_id)).data
       }
+      case 'tmdb/movie': {
+        return (await api.get('external_db/tmdb/movie?id=' + item_id)).data
+      }
+      case 'tmdb/tv': {
+        return (await api.get('external_db/tmdb/tv?id=' + item_id)).data
+      }
       default:
         return {}
     }
