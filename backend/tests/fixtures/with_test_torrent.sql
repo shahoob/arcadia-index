a0df925e7df65a0c3f97e73c02e7e9bd9758ab25
diceypotato <nice.mouse1085@fastmail.com>
2025-04-14 10:45:59 -0500

    tracker: stand-up a basic tracker skeleton
    The "tracker" portion of Arcadia is responsible for implementing the
    bittorrent announce protocol.  This stands up some basic machinery and
    testing for deserializing announce messages from torrent clients,
    mapping them to both users and torrents.
    
    There is still plenty to do:
    
      - Accounting for transported upload/download data
      - Aggregate accounting for snatch/completed/etc state at torrent level
      - Aggregate accounting for transfered data at the user level
      - Manage any freeleech/credit/etc state per-torrent
      - Provide endpoint for querying peer state
      - Inject user passkey into torrent download
      - Update fixtures.sql with real info_hashes
      - Lift much of the peer state into memory and out of postgres for
        speed (with some profiling)
      - Ensure great testing of ^

---
+++ backend/tests/fixtures/with_test_torrent.sql
@@ -0,0 +1,20 @@
+INSERT INTO title_groups VALUES (1, NULL, 'Love Me Do / P.S. I Love You', '{""}', '2025-03-30 16:35:06.418293', '2025-03-30 16:35:06.418293', 1, 'Tracklist
+A - Love Me Do
+B - P.S. I Love You', 'English', '1962-01-01 00:00:00', NULL, '{rock,pop}', 'UK', '{https://ia903406.us.archive.org/16/items/mbid-20e0bad7-bfbf-4f18-b0b3-8549dfcef6f3/mbid-20e0bad7-bfbf-4f18-b0b3-8549dfcef6f3-2190513301.jpg}', '{https://musicbrainz.org/release-group/5db85281-934d-36e5-865c-1922ad82a948,https://www.discogs.com/master/1154826-The-Beatles-Love-Me-Do}', NULL, 'Single', 'Music', NULL, NULL);
+
+INSERT INTO edition_groups VALUES (1, 1, 'red label Parlophone single', '1962-01-01 00:00:00', '2025-03-30 16:43:28.474757', '2025-03-30 16:43:28.474757', 1, 'Red label with silver print. Catalog number with 45 prefix. These labels have “PARLOPHONE” in large stylized letters at the top and the Parlophone £ logo at 3 o’clock.
+The publishing year was printed on the label, as “RECORDING FIRST PUBLISHED 1962″.
+This release has labels without “MADE IN GT. BRITAIN” printed below the Parlophone £ logo.
+Matrix numbers: Side A: 7XCE 17144-1N, Side B: 7XCE 17145-1N.', 'Parlophone', '{""}', '{https://www.discogs.com/release/1789539-The-Beatles-Love-Me-Do}', 'Vinyl', '{}');
+
+INSERT INTO torrents VALUES (1, 1, '2025-03-30 16:44:41.458969', '2025-03-30 16:44:41.458969', 1, '\x112233445566778899aabbccddeeff0011223344', NULL, 'The Beatles - Love Me Do - P.S. I Love You (Parlophone Single) [24-96]', '', 'Full scans included
+
+Lineage:
+
+Original red label Parlophone single > Numark TTXUSB (45.11 RPM) > Shure M97XE/Jico SAS > WAV > Click Repair, conversion to mono, EQ and normalization
+
+
+A notable needledrop since no tape source exists for the "Ringo version" of Love Me Do - all later releases are ultimately sourced from the original red label Parlophone single
+Some official releases are even needledrops of needledrops!
+
+This copy is far from perfect condition, but hopefully you find it listenable.', '{"png": 6, "txt": 1, "flac": 2}', false, '{"files": [{"name": "01 Love Me Do.flac", "size": 28507764}, {"name": "02 P.S. I Love You.flac", "size": 24491889}, {"name": "Notes.txt", "size": 492}, {"name": "Scans/Both A.png", "size": 118490649}, {"name": "Scans/Both B.png", "size": 117217894}, {"name": "Scans/Record A.png", "size": 94433739}, {"name": "Scans/Record B.png", "size": 93792174}, {"name": "Scans/Sleeve A.png", "size": 112202145}, {"name": "Scans/Sleeve B.png", "size": 112577343}], "parent_folder": "The Beatles - Love Me Do - P.S. I Love You (Parlophone Single) [24-96]"}', 'none', '', false, 'FLAC', 701714089, NULL, 'flac', 650, '24bit Lossless', NULL, NULL, '{Booklet}', '{""}', NULL);
