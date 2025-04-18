a72974ebfa08b0120ad0b11a450fbbd69a6f1305
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-04-18 17:27:20 +0200

    add series name in header for clarity

--- frontend/src/components/title_group/TitleGroupSlimHeader.vue
+++ frontend/src/components/title_group/TitleGroupSlimHeader.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="title">
-    {{ title_group.name }} <span>({{ title_group.original_release_date.substring(0, 4) }})</span>
+    <span v-if="title_group.series?.id">
+      <a class="series-name" :href="`/series?id=${title_group.series.id}`"
+        >{{ title_group.series.name }}
+      </a>
+      -
+    </span>
+    <span class="title-group-name">{{ title_group.name }}</span>
+    <span class="year">({{ title_group.original_release_date.substring(0, 4) }})</span>
   </div>
 </template>
 <script lang="ts">
@@ -13,7 +20,13 @@ export default {
 </script>
 <style scoped>
 .title {
-  font-weight: bold;
   font-size: 2em;
+}
+.title-group-name {
+  font-weight: bold;
+  margin-right: 5px;
+}
+.series-name {
+  font-weight: bold;
 }
 </style>
