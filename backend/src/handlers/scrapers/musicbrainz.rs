dc73b47c380093b922d0558f2d94f8ce5965b26e
Shehab Ali Tweel <shehabtweel@gmail.com>
2025-04-22 13:14:04 +0200

    another checkpoint

--- backend/src/handlers/scrapers/musicbrainz.rs
+++ backend/src/handlers/scrapers/musicbrainz.rs
@@ -4,44 +4,40 @@ use crate::{
 use actix_web::{HttpResponse, web};
 use serde::Deserialize;
 
-use musicbrainz_rs::entity::{
+use musicbrainz_rs::{client::MUSICBRAINZ_CLIENT, entity::{
     artist::Artist, coverart::Coverart, release::Release, release_group::{ReleaseGroup, ReleaseGroupPrimaryType}
-};
+}};
 use musicbrainz_rs::prelude::*;
+
+// TODO: Make this configurable incase user wants different musicbrainz instance
+const MUSICBRAINZ_WEB_SERVICE_URL: &str = "https://musicbrainz.org";
 
 #[derive(Debug, Deserialize)]
 struct MusicBrainzQuery {
     id: String,
 }
 
-impl TryFrom<ReleaseGroupPrimaryType> for TitleGroupCategory {
-    fn try_from(value: ReleaseGroupPrimaryType) -> Result<Self> {
-        match value {
-            ReleaseGroupPrimaryType::Album => Ok(Self::Album),
-            ReleaseGroupPrimaryType::Ep => Ok(Self::Ep),
-            ReleaseGroupPrimaryType::Single => Ok(Self::Single),
-            ReleaseGroupPrimaryType::Broadcast => todo!(),
-            ReleaseGroupPrimaryType::Other => Ok(Self::Other),
-        }
-    }
-}
-
 pub async fn get_musicbrainz_release_group_info(query: web::Query<MusicBrainzQuery>) -> Result<HttpResponse> {
     let release_group = ReleaseGroup::fetch()
     .id(&query.id)
-    .execute().await?;
+    .execute().await.expect("Couldn't fetch release group");
 
-    let primary_type = release_group.primary_type;
+    let mut urls: Vec<String> = vec![format!("{}/release_group/{}", MUSICBRAINZ_WEB_SERVICE_URL, query.id)];
+
+    // release_group.relations
 
     let title_group = UserCreatedTitleGroup {
         name: release_group.title,
 
-        external_links: vec![todo!("will put musicbrainz release group url and other external links found in it")],
+        external_links: urls,
 
         content_type: ContentType::Music,
-        category: if primary_type.is_some() { TitleGroupCategory::try_from(primary_type.unwrap()) } else { Some(TitleGroupCategory::Other) },
+        category: todo!(),
         ..create_default_title_group()
     };
 
-    todo!("Not enough info is programmed to be retrived yet from MusicBrainz")
+    
+    todo!("Not enough info is programmed to be retrived yet from MusicBrainz");
+
+    // Ok(HttpResponse::Ok().json(serde_json::json!({"title_group": title_group})));
 }
