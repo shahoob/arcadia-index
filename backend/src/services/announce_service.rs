5b0ce4284c1f70005b98b029bf8eeaf66f1afb52
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-04-23 18:19:46 +0200

    actually, peer_id isn't always the same length

--- backend/src/services/announce_service.rs
+++ backend/src/services/announce_service.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 pub fn is_torrent_client_allowed(
     peer_id: &[u8; 20],
-    allowed_torrent_clients: &HashSet<[u8; 4]>,
+    allowed_torrent_clients: &HashSet<Vec<u8>>,
 ) -> bool {
     let peer_id_without_hyphen = &peer_id[1..];
     for prefix in allowed_torrent_clients.iter() {
