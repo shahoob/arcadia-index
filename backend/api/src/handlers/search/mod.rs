3efc6490bc159e344a200757ea4f79299781d4d1
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-09-01 18:01:53 +0200

    fixed wrong http verbs

--- backend/api/src/handlers/search/mod.rs
+++ backend/api/src/handlers/search/mod.rs
@@ -4,18 +4,18 @@ pub mod search_title_group_info_lite;
 pub mod search_torrent_requests;
 pub mod search_torrents;
 
-use actix_web::web::{post, resource, ServiceConfig};
+use actix_web::web::{get, post, resource, ServiceConfig};
 use arcadia_storage::redis::RedisPoolInterface;
 
 pub fn config<R: RedisPoolInterface + 'static>(cfg: &mut ServiceConfig) {
     cfg.service(
         resource("/title-groups/lite")
-            .route(post().to(self::search_title_group_info_lite::exec::<R>)),
+            .route(get().to(self::search_title_group_info_lite::exec::<R>)),
     );
     cfg.service(resource("/torrents/lite").route(post().to(self::search_torrents::exec::<R>)));
     cfg.service(resource("/artists/lite").route(post().to(self::search_artists_lite::exec::<R>)));
     cfg.service(
-        resource("/torrent-requests").route(post().to(self::search_torrent_requests::exec::<R>)),
+        resource("/torrent-requests").route(get().to(self::search_torrent_requests::exec::<R>)),
     );
     cfg.service(resource("/forum/threads").route(post().to(self::search_forum_thread::exec::<R>)));
 }
