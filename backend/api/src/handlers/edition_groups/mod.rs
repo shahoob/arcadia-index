852869b6f9c08abd400474ed1140c2c19b5976c1
Shane Volpe <shanevolpe@gmail.com>
2025-09-02 16:42:02 -0400

    Fix wrong HTTP method from create edition: change from get to post (#351)

--- backend/api/src/handlers/edition_groups/mod.rs
+++ backend/api/src/handlers/edition_groups/mod.rs
@@ -1,7 +1,7 @@
 pub mod create_edition_group;
-use actix_web::web::{get, resource, ServiceConfig};
+use actix_web::web::{post, resource, ServiceConfig};
 use arcadia_storage::redis::RedisPoolInterface;
 
 pub fn config<R: RedisPoolInterface + 'static>(cfg: &mut ServiceConfig) {
-    cfg.service(resource("").route(get().to(self::create_edition_group::exec::<R>)));
+    cfg.service(resource("").route(post().to(self::create_edition_group::exec::<R>)));
 }
