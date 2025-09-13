73e3b67024074d7c20db5544b20bd62560da7d59
FrenchGithubUser <frenchgithubuser@tutanota.com>
2025-09-13 12:13:35 +0200

    split torrent requests bounties into their own columns, added more translations

--- frontend/src/components/torrent_request/TorrentRequestsTable.vue
+++ frontend/src/components/torrent_request/TorrentRequestsTable.vue
@@ -1,6 +1,6 @@
 <template>
   <DataTable :value="torrentRequests" size="small">
-    <Column header="Title" v-if="displayTitleGroup">
+    <Column :header="t('general.title')" v-if="displayTitleGroup">
       <template #body="slotProps">
         <TitleGroupSlimHeader
           :titleGroup="slotProps.data.title_group"
@@ -9,17 +9,20 @@
         />
       </template>
     </Column>
-    <Column header="Requirements">
+    <Column :header="t('torrent_request.requirement', 2)">
       <template #body="slotProps">
         <RouterLink :to="`/torrent-request/${slotProps.data.torrent_request.id}`">
           <TorrentRequestSlug :torrentRequest="slotProps.data.torrent_request" :contentType="contentType ?? slotProps.data.title_group.content_type" />
         </RouterLink>
       </template>
     </Column>
-    <Column header="Bounty">
-      <template #body="slotProps"> {{ bytesToReadable(slotProps.data.bounty.upload) }} + {{ slotProps.data.bounty.bonus_points }} bp </template>
+    <Column :header="t('user.upload')">
+      <template #body="slotProps">{{ bytesToReadable(slotProps.data.bounty.upload) }}</template>
     </Column>
-    <Column header="Voters">
+    <Column :header="t('user.bonus_points')">
+      <template #body="slotProps">{{ slotProps.data.bounty.bonus_points }} bp</template>
+    </Column>
+    <Column :header="t('torrent_request.voter', 2)">
       <template #body="slotProps"> {{ slotProps.data.user_votes_amount }} </template>
     </Column>
   </DataTable>
@@ -33,11 +36,14 @@ import TitleGroupSlimHeader from '../title_group/TitleGroupSlimHeader.vue'
 import { RouterLink } from 'vue-router'
 import type { ContentType } from '@/services/api/torrentService'
 import { bytesToReadable } from '@/services/helpers'
+import { useI18n } from 'vue-i18n'
 
 defineProps<{
   torrentRequests: TorrentRequestHierarchyLite[] | TorrentRequestWithTitleGroupLite[]
   contentType?: ContentType
   displayTitleGroup?: boolean
 }>()
+
+const { t } = useI18n()
 </script>
 <style scoped></style>
